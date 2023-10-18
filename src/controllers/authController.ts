9ef5bb4d0478194b3b7c2ca18e48d3643dad32e3
Ahanaf Islam <ahanafislamworks@gmail.com>
2023-10-19 05:21:43 +0600

    trpc i and prisma chngd

---
+++ src/controllers/authController.ts
@@ -0,0 +1,37 @@
+import { getServerSession } from "next-auth/next";
+import { TRPCError } from "@trpc/server";
+import db from "@/db";
+
+export const authController = async () => {
+  const session = await getServerSession();
+
+  if (!session?.user?.email || !session?.user?.name) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "You must be logged in to do this",
+    });
+  }
+
+  const dbuser = await db.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (!dbuser) {
+    await db.user.create({
+      data: {
+        password: "From Email",
+        email: session.user.email,
+        name: session.user.name,
+      },
+    });
+  }
+  // after create get the userinfo
+  const fetchedUser = await db.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+  return { sucess: true, user: fetchedUser };
+};
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,11 +1,30 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { useState } from "react";
+import { QueryClient } from "@tanstack/react-query";
+import { trpc } from "@/app/_trpc/client";
+import { httpBatchLink } from "@trpc/client";
 
 type Props = {
-  children?: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export const NextAuthProvider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000/api/trpc",
+        }),
+      ],
+    })
+  );
+
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </trpc.Provider>
+  );
 };
