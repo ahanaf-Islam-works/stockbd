ce6a899338886d2d231afec8206e99f7e165f0d4
Ahanaf Islam <ahanafislamworks@gmail.com>
2023-10-18 21:59:51 +0600

    okay

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,16 +1,33 @@
-import Wrapper from "../Wrapper";
+import Profile from "./Profile";
+import Link from "next/link";
+import { MessageSquare } from "lucide-react";
 export default function Dashboard() {
   return (
-    <section className="bg-slate-300 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-10 gap-5 font-light mx-5 min-h-screen rounded-md mt-4 mb-4 p-2">
-      <div className="min-h-full bg-slate-500 rounded-md lg:col-span-2 col-span-12 m-auto w-full sm:w-full">
-        <h1>Profile</h1>
+    <section className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-10 gap-5 font-light mx-5 min-h-screen rounded-md mt-4 mb-4 p-2">
+      <div className="min-h-full shadow rounded-md lg:col-span-2 col-span-12 m-auto w-full sm:w-full p-4">
+        <Profile />
+
+        <Link
+          href="/"
+          className="shadow-sm bg-white p-6 rounded flex flex-col mb-7 hover:shadow-lg"
+        >
+          <div className="flex items-center gap-4">
+            <MessageSquare
+              size={40}
+              className="border-2 border-cyan-950 rounded-full p-2"
+            />
+            <p className="text-base font-semibold text-zinc-700">
+              Customer Support
+            </p>
+          </div>
+        </Link>
       </div>
 
-      <div className="min-h-full bg-slate-500 rounded-md lg:col-span-4 col-span-12 m-auto w-full sm:w-full">
+      <div className="min-h-full rounded-md lg:col-span-4 col-span-12 m-auto w-full sm:w-full p-4">
         <h1>Markets Overview</h1>
       </div>
 
-      <div className="min-h-full bg-slate-500 rounded-md lg:col-span-4 col-span-12 m-auto w-full sm:w-full">
+      <div className="min-h-full rounded-md lg:col-span-4 col-span-12 m-auto w-full sm:w-full p-4">
         <h1>Portfolio</h1>
       </div>
     </section>
---
+++ src/components/dashboard/Profile.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import {
+  User,
+  LayoutDashboard,
+  DollarSign,
+  Settings,
+  Receipt,
+  ArrowLeftRight,
+} from "lucide-react";
+
+export default function Profile() {
+  return (
+    <aside className="bg-white p-6 rounded shadow-sm flex flex-col mb-7">
+      <div className="flex items-center gap-4">
+        <User size={30} className="border-2 border-cyan-950 rounded-full" />
+        <p className="text-base font-semibold text-zinc-700">John Doe</p>
+      </div>
+      <div className="flex items-center gap-4 mb-10 border-b-2 pb-4 border-zinc-500">
+        <DollarSign
+          size={30}
+          className="border-2 border-cyan-950 rounded-full"
+        />
+        <p className="text-base font-semibold text-zinc-700">
+          Balance <br />
+          <span className="font-bold text-zinc-600">100000.00</span>
+        </p>
+      </div>
+
+      <ul className="list-none">
+        <li className="mb-4">
+          <Link href="#" className="text-cyan-950 hover:underline text-sm">
+            <LayoutDashboard size={25} className="inline-block mr-2" />{" "}
+            Dashboard
+          </Link>
+        </li>
+        <li className="mb-4">
+          <Link href="#" className="text-cyan-950 hover:underline text-sm">
+            <ArrowLeftRight size={25} className="inline-block mr-2" />{" "}
+            Transactions
+          </Link>
+        </li>
+        <li className="mb-4">
+          <Link href="#" className="text-cyan-950 hover:underline text-sm">
+            <Settings size={25} className="inline-block mr-2" /> Settings &amp;
+            Security
+          </Link>
+        </li>
+        <li className="mb-4">
+          <Link href="#" className="text-cyan-950 hover:underline text-sm">
+            <Receipt size={25} className="inline-block mr-2" /> Billing
+          </Link>
+        </li>
+      </ul>
+    </aside>
+  );
+}
