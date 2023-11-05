8c2222b93bad3b4c0f8d3940bece739b51906367
Ahanaf Islam <ahanafislamworks@gmail.com>
2023-11-05 13:37:21 +0600

    test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,20 +1,10 @@
 import Wrapper from "@/components/Wrapper";
-import SingleStockView from "@/components/singleStock/SingleStockView";
-import { serverClient } from "@/trpc/serverClient";
-
+import { Test } from "@/components/test/Test";
 const page = async () => {
-  const data = await serverClient.user.getSingleStockData("IBM");
-  const graph = data.output;
-  const meta = data.metaData;
   return (
     <Wrapper full>
-      <div className="text-zinc-700 col-span-2">
-        <h3>
-          Price of {meta["2. Symbol"]} in {meta["6. Time Zone"]}
-        </h3>
-      </div>
-      <div className="col-span-4">
-        <SingleStockView graph={graph} />
+      <div>
+        <Test />
       </div>
     </Wrapper>
   );
